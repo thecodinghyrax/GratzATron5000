85af8c4cf0d7bb674a434ab7a864ff8d33840334
thecodinghyrax <drewxcom@gmail.com>
2016-02-14 16:55:18 -0600

    Adding in main files
    Adding the main addon files to the repo

---
+++ core.lua
@@ -0,0 +1,100 @@
+print("Type /gat5 to show the GratzATron5000 if you have closed it.")
+SLASH_GratzATron5000Show1 = "/gat5"
+SlashCmdList["GratzATron5000Show"] = function()
+  GratzFrame:Show()
+end
+
+function MainFrame_OnVarLoad(this)
+    this:ClearAllAnchors();
+    if not MainFrame_X or not MainFrame_Y then
+        this:SetAnchor("CENTER", "CENTER", "UIParent", 0, 0);
+	else
+	   local scale = GetUIScale();
+	   this:SetAnchor("TOPLEFT", "TOPLEFT", "UIParent", MainFrame_X / scale, MainFrame_Y / scale);
+    end
+end
+
+function MainFrame_MoveEnd()
+    MainFrame:StopMovingOrSizing();
+    MainFrame_X, MainFrame_Y = MainFrame:GetPos();
+end
+
+function superGratz()
+SendChatMessage("(Drew): " .. lvlThree[math.random(0,table.getn(lvlThree))], "guild" );
+end
+
+function bigGratz()
+SendChatMessage("(Drew): " .. lvlTwo[math.random(0,table.getn(lvlTwo))], "guild" );
+end
+
+function gratz()
+SendChatMessage("(Drew): " .. lvlOne[math.random(0,table.getn(lvlOne))], "guild" );
+end
+
+
+
+local MainFrame = CreateFrame("Frame", "GratzFrame", UIParent, "BasicFrameTemplateWithInset");
+MainFrame:SetSize(120, 140); --width, height
+MainFrame:SetPoint("TOP", UIParent, "CENTER", 0, 300); --point, relativeFrame, relativePoint, xOffset, yOffset
+MainFrame:EnableMouse(true)
+MainFrame:SetMovable(true);
+MainFrame:RegisterForDrag("LeftButton")
+MainFrame:SetScript("OnDragStart",MainFrame.StartMoving)
+MainFrame:SetScript("OnDragStop",function(self)
+ self:StopMovingOrSizing()
+ settings.XPos = self:GetLeft()
+ settings.YPos = self:GetBottom()
+end)
+
+MainFrame:SetScript("OnEvent",function(self,event,...)
+ if event=="PLAYER_LOGIN" then
+ MyAddonPerSettings = GratzATron5000PerSettings or {} -- create table if one doesn't exist
+ settings = GratzATron5000PerSettings -- assign settings declared above
+ if settings.XPos then
+ MainFrame:ClearAllPoints()
+ MainFrame:SetPoint("BOTTOMLEFT",settings.XPos,settings.YPos)
+ end
+ end
+end)
+
+--Child frames and regions:
+
+--UI Title:
+MainFrame.title = MainFrame:CreateFontString(nil, "OVERLAY");
+MainFrame.title:SetFontObject("GameFontNormalSmall");
+MainFrame.title:SetPoint("LEFT", MainFrame.TitleBg, "LEFT", 5, 0);
+MainFrame.title:SetText("GratzATron5000");
+
+--UI Super Gratz button:
+MainFrame.SuperGratz = CreateFrame("Button", "SuperGratz", MainFrame, "GameMenuButtonTemplate");
+MainFrame.SuperGratz:SetPoint("BOTTOM", MainFrame, "TOP", 0, -62);
+MainFrame.SuperGratz:SetSize(100, 35);
+MainFrame.SuperGratz:SetText("Super Gratz");
+MainFrame.SuperGratz:SetNormalFontObject("GameFontNormalSmall");
+MainFrame.SuperGratz:SetHighlightFontObject("GameFontHighlightMedium");
+MainFrame.SuperGratz:SetScript("OnClick", function (self, button, down)
+  superGratz();
+end);
+
+
+--UI Big Gratz Button
+MainFrame.bigGratz = CreateFrame("Button", nil, MainFrame, "GameMenuButtonTemplate");
+MainFrame.bigGratz:SetPoint("TOP", MainFrame.SuperGratz, "BOTTOM", 0, 0);
+MainFrame.bigGratz:SetSize(100, 35);
+MainFrame.bigGratz:SetText("Big Gratz");
+MainFrame.bigGratz:SetNormalFontObject("GameFontNormalSmall");
+MainFrame.bigGratz:SetHighlightFontObject("GameFontHighlightLarge");
+MainFrame.bigGratz:SetScript("OnClick", function (self, button, down)
+  bigGratz();
+end);
+
+--UI Gratz Button
+MainFrame.gratz = CreateFrame("Button", nil, MainFrame, "GameMenuButtonTemplate");
+MainFrame.gratz:SetPoint("TOP", MainFrame.bigGratz, "BOTTOM", 0, 0);
+MainFrame.gratz:SetSize(100, 35);
+MainFrame.gratz:SetText("Gratz");
+MainFrame.gratz:SetNormalFontObject("GameFontNormalSmall");
+MainFrame.gratz:SetHighlightFontObject("GameFontHighlightLarge");
+MainFrame.gratz:SetScript("OnClick", function (self, button, down)
+  gratz();
+end);
---
+++ gratzData.lua
@@ -0,0 +1,28 @@
+lvlThree = {
+  "{rt1}{rt1}Super Gratz{rt1}{rt1}",
+  "Thats Fantastic. Great Job!!!",
+  "Dang...You Rock!!!",
+  "BIG OL GRATZ!!!",
+  "{rt3}Mega Gratz{rt3}",
+  "Super Ultra Mega Gratz!!!",
+  "W00t W00t Awesome Job!!!"
+}
+
+lvlTwo = {
+  "Great Job",
+  "Nice, congratulations",
+  "Job well done",
+  "Awesomesause",
+  'You have "Achieved" much ;)',
+  "Congratulations to you on your achievement :D"
+}
+
+lvlOne = {
+  "Gratz",
+  "Congratulations",
+  "Nice",
+  "Good Job",
+  "Conga-rats",
+  "w00t",
+  "Way to go!",
+}
